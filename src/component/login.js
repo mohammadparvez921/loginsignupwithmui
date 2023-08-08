19dea2e99d1699decdf9d4ff4e2b4e00580f0fe5
mohammadparvez921 <parrymd786@gmail.com>
2023-08-08 20:19:30 +0530

    responsivess added

--- src/component/login.js
+++ src/component/login.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import {  Button, Grid, Paper, TextField, Typography } from '@material-ui/core'
+import { Button, Grid, Paper, TextField, Typography } from '@material-ui/core'
 import { Link } from 'react-router-dom';
+import loginImg from '../img/loginFrame.png'
+import './login.css'
 
 function login() {
     const paperStyle = {
@@ -11,32 +13,58 @@ function login() {
     const headerStyle = {
         margin: 0,
     }
-    
-  return (
-    <div>
-            <Grid>
-                <Paper elevation={20} style={paperStyle}>
-                    <Grid align='center'>          
-                    <h2 style={headerStyle}>Log in</h2>
-                        <Typography variant='caption' style={{fontWeight:"bold"}}>Please fill the deatils to login!</Typography>
-                    </Grid>
+    const marginTop = {
+        marginTop: 10,
+    }
 
-                    <form>
-                        <TextField fullWidth label='Email' placeholder='Enter your Email' />
-                        <TextField fullWidth label='Password' placeholder='Enter your Password' />
+    return (
+        <>
+            <Grid container spacing={2} alignItems="center">
 
-                        <Button type='submit' variant='contained' color='primary'>Log in</Button>
-                    </form>
-                    <span>Not registered? <Link to="/" style={{
-          textDecoration: 'none',
-          fontWeight: 'bold',
-        }}>SignUp</Link></span>
-                </Paper>
+               
+
+
+
+
+
+
+
+
+
+                <Grid item xs={12} sm={6}>
+                    <Paper elevation={20} style={paperStyle}>
+                        <Grid align='center'>
+                            <h2 style={headerStyle}>Log in</h2>
+                            <Typography variant='caption' style={{ fontWeight: "bold" }}>Please fill the deatils to login!</Typography>
+                        </Grid>
+
+                        <form>
+                            <TextField fullWidth label='Email' placeholder='Enter your Email' />
+                            <TextField fullWidth label='Password' placeholder='Enter your Password' />
+
+                            <Button type='submit' variant='contained' color='primary' style={marginTop}>Log in</Button>
+                        </form>
+                        <span>Not registered? <Link to="/" style={{
+                            textDecoration: 'none',
+                            fontWeight: 'bold',
+                        }}>SignUp</Link></span>
+                    </Paper>
+
+                </Grid>
+
+                 {/* Image Column */}
+                 <Grid item xs={12} sm={6}>
+                    <div className="login-image">
+                        <img src={loginImg} alt="Login" />
+                    </div>
+                </Grid>
+
 
             </Grid>
 
-        </div>
-  )
+
+        </>
+    )
 }
 
 export default login
--- src/component/signup.js
+++ src/component/signup.js
@@ -7,6 +7,8 @@ import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
 import PersonAddAlt1OutlinedIcon from '@mui/icons-material/PersonAddAlt1Outlined';
 import { Link } from 'react-router-dom';
+import signupImg from '../img/registerFrame.png'
+import './signup.css'
 
 import Checkbox from '@mui/material/Checkbox';
 function signup() {
@@ -22,60 +24,74 @@ function signup() {
         marginTop: 5,
     }
     return (
-        <div>
-            <Grid>
-                <Paper elevation={20} style={paperStyle}>
-                    <Grid align='center'>
-                        <Avatar>
-                           <PersonAddAlt1OutlinedIcon/>
-                        </Avatar>
-                        <h2 style={headerStyle}>Sign Up</h2>
-                        <Typography variant='caption' style={{fontWeight:"bold"}}>Please fill yhe deatils to signup!</Typography>
-                    </Grid>
-
-                    <form>
-                        <TextField fullWidth label='Name' placeholder='Enter your Name' />
-                        <TextField fullWidth label='Email' placeholder='Enter your Email' />
+        <>
+            <Grid container spacing={2} alignItems="center">
+                
 
 
-                        <FormControl style={marginTop}>
-                            <FormLabel id="demo-row-radio-buttons-group-label">Gender</FormLabel>
-                            <RadioGroup
-                                row
-                                aria-labelledby="demo-row-radio-buttons-group-label"
-                                name="row-radio-buttons-group"
-                            >
-                                <FormControlLabel value="female" control={<Radio />} label="Female" />
-                                <FormControlLabel value="male" control={<Radio />} label="Male" />
-                                <FormControlLabel value="other" control={<Radio />} label="Other" />
+                <Grid item xs={12} sm={6}>
+                    <Paper elevation={20} style={paperStyle}>
+                        <Grid align='center'>
+                            <Avatar>
+                                <PersonAddAlt1OutlinedIcon />
+                            </Avatar>
+                            <h2 style={headerStyle}>Sign Up</h2>
+                            <Typography variant='caption' style={{ fontWeight: "bold" }}>Please fill yhe deatils to signup!</Typography>
+                        </Grid>
 
-                            </RadioGroup>
-                        </FormControl>
-
-
+                        <form>
+                            <TextField fullWidth label='Name' placeholder='Enter your Name' />
+                            <TextField fullWidth label='Email' placeholder='Enter your Email' />
 
 
+                            <FormControl style={marginTop}>
+                                <FormLabel id="demo-row-radio-buttons-group-label">Gender</FormLabel>
+                                <RadioGroup
+                                    row
+                                    aria-labelledby="demo-row-radio-buttons-group-label"
+                                    name="row-radio-buttons-group"
+                                >
+                                    <FormControlLabel value="female" control={<Radio />} label="Female" />
+                                    <FormControlLabel value="male" control={<Radio />} label="Male" />
+                                    <FormControlLabel value="other" control={<Radio />} label="Other" />
+
+                                </RadioGroup>
+                            </FormControl>
 
 
-                        <TextField fullWidth label='Phone Number' placeholder='Enter your Phone Number' />
-                        <TextField fullWidth label='Password' placeholder='Enter your Password' />
-                        <TextField fullWidth label='Confirm Password' placeholder='Confirm your Password' />
-
-                        <FormControlLabel required control={<Checkbox />} label="I accept the terms and conditions" />
 
 
-                        <Button type='submit' variant='contained' color='primary'>Sign Up</Button>
-                    </form>
-                    <span>Already signed up <Link to="/login" style={{
-          textDecoration: 'none',
-          fontWeight: 'bold',
-        }}>Login</Link></span>
-                </Paper>
 
+
+                            <TextField fullWidth label='Phone Number' placeholder='Enter your Phone Number' />
+                            <TextField fullWidth label='Password' placeholder='Enter your Password' />
+                            <TextField fullWidth label='Confirm Password' placeholder='Confirm your Password' />
+
+                            <FormControlLabel required control={<Checkbox />} label="I accept the terms and conditions" />
+
+
+                            <Button type='submit' variant='contained' color='primary'>Sign Up</Button>
+                        </form>
+                        <span>Already signed up <Link to="/login" style={{
+                            textDecoration: 'none',
+                            fontWeight: 'bold',
+                        }}>Login</Link></span>
+                    </Paper>
+
+                </Grid>
+
+                {/* Image Column */}
+                <Grid item xs={12} sm={6}>
+                    <div className="signup-image">
+                        <img src={signupImg} alt="Login" />
+                    </div>
+                </Grid>
             </Grid>
 
-        </div>
+        </>
     )
 }
 
 export default signup
+
+
